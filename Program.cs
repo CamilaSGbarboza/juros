a86060cc4429e1376af818889836be4bfce66598
camila <camilasangobar@gmail.com>
2021-04-03 00:06:10 -0300

    Juros

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace juros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("------Juros Simples------");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Capital (R$):");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Taxa de juros (%):");
+            double i = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Tempo (meses):");
+            double t = Convert.ToDouble(Console.ReadLine());
+
+            double j = c * t * i;
+            double m = c + j;
+
+            Console.WriteLine();
+            Console.WriteLine($"Juros (R$):{j}");
+            Console.WriteLine($"Montante (R$):{m}");
+
+
+
+
+
+        }
+    }
+}
